b552a16ba6270f4c12ff87c539a7ea850288c9ce
henrikebraga <ghostdarko356@gmail.com>
2026-02-17 15:44:56 -0300

    dia 09: criar app tarefas com startapp

---
+++ tarefas/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+# Essa classe identifica o app pro Django
+class TarefasConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tarefas'
