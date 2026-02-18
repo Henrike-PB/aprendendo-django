11daa2b101d94b1156b5485b11c5e35f81eea0d4
henrikebraga <ghostdarko356@gmail.com>
2026-02-18 18:42:07 -0300

    dia 11: criar primeira view com httpresponse

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# Essa função é a view — o "atendente" do departamento
+# Ela recebe um pedido (request) e devolve uma resposta
+def index(request):
+    return HttpResponse("Henrike está aprendendo Django! 🚀")
